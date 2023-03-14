dd2535b4b882c0703fee49395f4e8dbeb5490283
Lurzar_ <68995865+lurzar@users.noreply.github.com>
2023-03-14 19:16:20 +0800

    Added expenses page

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ExpensesController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -23,9 +24,12 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    // Profile
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    // Expenses
+    Route::get('/expenses', [ExpensesController::class, 'index'])->name('expenses.index');
 });
 
 require __DIR__.'/auth.php';
