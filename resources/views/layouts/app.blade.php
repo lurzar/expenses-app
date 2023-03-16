abe0486e64c9c444c4b5f9f4b4d95ec9f80d5957
Lurzar_ <68995865+lurzar@users.noreply.github.com>
2023-03-17 07:20:48 +0800

    Include livewire script

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,8 +13,10 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @livewireStyles
     </head>
     <body class="font-sans antialiased">
+        @livewireScripts
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
             @include('layouts.navigation')
 
