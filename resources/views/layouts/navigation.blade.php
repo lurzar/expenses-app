3f3d1ce2e7fb3ee69931ae9e44f0617dc6bef3cf
Lurzar_ <68995865+lurzar@users.noreply.github.com>
2023-03-20 05:40:10 +0800

    Updated navigation menu

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,13 +15,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
-                </div>
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('expenses.index')" :active="request()->routeIs('expenses.index')">
+                    <x-nav-link :href="route('planning.index')" :active="request()->routeIs('planning.index')">
                         {{ __('Planning') }}
                     </x-nav-link>
-                </div>
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('expenses.index')" :active="request()->routeIs('expenses.index')">
                         {{ __('Expenses') }}
                     </x-nav-link>
@@ -79,6 +75,12 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('planning.index')" :active="request()->routeIs('planning.index')">
+                {{ __('Planning') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('expenses.index')" :active="request()->routeIs('expenses.index')">
+                {{ __('Expenses') }}
             </x-responsive-nav-link>
         </div>
 
