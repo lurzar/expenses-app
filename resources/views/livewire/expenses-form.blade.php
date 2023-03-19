f5fafae5babb621d7e00a76079be5a92f0e04f8a
Lurzar_ <68995865+lurzar@users.noreply.github.com>
2023-03-20 05:16:16 +0800

    Minor changes

--- resources/views/livewire/expenses-form.blade.php
+++ resources/views/livewire/expenses-form.blade.php
@@ -126,7 +126,7 @@
     {{-- Planning summary --}}
     <header class="mb-6">
         <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
-            {{ __('Plannig Summary') }}
+            {{ __('Planning Summary') }}
         </h2>
 
         <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
