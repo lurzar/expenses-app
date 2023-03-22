06261add19814f7f6aefb2bb6bbb67b6872bfce0
Lurzar_ <68995865+lurzar@users.noreply.github.com>
2023-03-22 21:29:17 +0800

    Renamed get list month helper

--- app/helpers.php
+++ app/helpers.php
@@ -3,8 +3,8 @@
 use Carbon\Carbon;
 
 // Get list of months
-if (! function_exists('get_months')) {
-    function get_months() {
+if (! function_exists('getMonthList')) {
+    function getMonthList() {
         $data = collect();
 
         foreach (range(1, 12) as $month) {
