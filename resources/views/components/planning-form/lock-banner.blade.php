30bea73abdd725fe4daceefa1869e21fa74ce965
Lurzar_ <68995865+lurzar@users.noreply.github.com>
2023-03-24 19:16:03 +0800

    Updated props and minor changes

--- resources/views/components/planning-form/lock-banner.blade.php
+++ resources/views/components/planning-form/lock-banner.blade.php
@@ -1,14 +1,14 @@
-@props(['allowed_date', 'current_month_year'])
+@props(['open_date'])
 
 <div>
     {{-- Form locked --}}
     <header>
         <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
-            Form Locked !
+            Form Locked
         </h2>
 
         <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-            Please wait until {{ $allowed_date }} {{ $current_month_year }}.
+            Please wait until {{ $open_date }}.
         </p>
     </header>
 </div>
