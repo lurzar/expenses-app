4cc3d75c9717f57e2124945f04b360753160b175
Lurzar_ <68995865+lurzar@users.noreply.github.com>
2023-03-27 19:09:26 +0800

    Added saving information section

--- resources/views/livewire/planning-form.blade.php
+++ resources/views/livewire/planning-form.blade.php
@@ -56,6 +56,90 @@
         </div>
     </div>
 
+    {{-- Savings information --}}
+    <header class="mb-6">
+        <div class="grid grid-cols-2 gap-4">
+            <div>
+                <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+                    {{ __('Savings Information') }}
+                </h2>
+                <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+                    {{ __("Provide your savings information with appropriate amount.") }}
+                </p>
+            </div>
+            <div class="grid grid-cols-1 place-items-end">
+                <div>
+                    <x-text-input 
+                        id="balance_after_saving_rate" 
+                        class="block mt-1 dark:text-gray-500 text-right" 
+                        type="number" 
+                        name="balance_after_saving_rate" 
+                        min="0" 
+                        step="any" 
+                        wire:model="balance_after_saving_rate" 
+                        :value="old('balance_after_saving_rate')"  
+                        :disabled="true" 
+                    />
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <hr class="h-px my-6 w-80 bg-gray-200 border-0 dark:bg-gray-700">
+
+    <div class="grid grid-cols-6 gap-4 mb-6 items-end">
+        @forelse ($savings_items as $savings_item)   
+            <div class="col-span-4">
+                <x-input-label for="savings-{{ $savings_item }}" :value="__('Item')" />
+                <x-text-input 
+                    id="savings-{{ $savings_item }}" 
+                    class="block mt-1 w-full" 
+                    type="text" 
+                    name="savings.{{ $savings_item }}" 
+                    wire:model="savings_values.{{ $savings_item }}.item" 
+                    :value="old('savings_values.'.$savings_item.'.item')" 
+                    placeholder="e.g. Loans, Insurances" 
+                    required 
+                    autofocus 
+                />
+                <x-input-error :messages="$errors->get('savings_values.'.$savings_item.'.item')" class="mt-2" />
+            </div>
+            <div>
+                <x-input-label for="savings-amount-{{ $savings_item }}" :value="__('Amount (RM)')" />
+                <x-text-input 
+                    id="savings-amount-{{ $savings_item }}" 
+                    class="block mt-1 w-full" 
+                    type="number" 
+                    name="savings-amount.{{ $savings_item }}"
+                    wire:model="savings_values.{{ $savings_item }}.amount"  
+                    min="0.00" 
+                    step="any" 
+                    :value="old('savings_values.'.$savings_item.'.amount')" 
+                    placeholder="0.00" 
+                    required 
+                    autofocus 
+                />
+                <x-input-error :messages="$errors->get('savings_values.'.$savings_item.'.amount')" class="mt-2" />
+            </div>
+            @if ($savings_item != 0)
+                <div>
+                    <x-planning-form.item-remove-button wire:click="removeSavingItem({{ $savings_item }})">
+                        {{ __('Remove Item') }}
+                    </x-planning-form.item-remove-button>
+                </div>
+            @endif
+        @empty
+            <div class="col-span-5 text-red-700 dark:text-red-500">
+                {{ __('Error occured while fetching savings items field') }}
+            </div>
+        @endforelse
+    </div>
+    <div class="mb-10">
+        <x-planning-form.item-add-button wire:click="addSavingItem({{ $savings_items }})">
+            {{ __('Add Item') }}
+        </x-planning-form.item-add-button>
+    </div>
+
     {{-- Commitment expenses information --}}
     <header class="mb-6">
         <div class="grid grid-cols-2 gap-4">
