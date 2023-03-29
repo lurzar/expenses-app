6bca79208e34161db71c128dd7fcb0462022d01d
Lurzar_ <68995865+lurzar@users.noreply.github.com>
2023-03-29 19:12:52 +0800

    Added section component

---
+++ resources/views/components/planning-form/section-header.blade.php
@@ -0,0 +1,15 @@
+@props(['title' => '', 'description' => ''])
+
+<div>
+    <header class="mb-6">
+        <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+            {{ $title }}
+        </h2>
+
+        <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+            {{ $description }}
+        </p>
+    </header>
+
+    <hr class="h-px my-6 w-80 bg-gray-200 border-0 dark:bg-gray-700">
+</div>
