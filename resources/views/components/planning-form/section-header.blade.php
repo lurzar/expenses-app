853f4b2870caeb1515c1dbd047420177fe64f415
Lurzar_ <68995865+lurzar@users.noreply.github.com>
2023-03-29 19:23:11 +0800

    Updated section header component

--- resources/views/components/planning-form/section-header.blade.php
+++ resources/views/components/planning-form/section-header.blade.php
@@ -1,14 +1,42 @@
-@props(['title' => '', 'description' => '', 'total_saving' => 0, 'balance' => 0, 'type' => 'standard'])
+@props(['title' => '', 'description' => '', 'total_saving' => 0, 'balance' => 0, 'custom' => false])
 
 <div>
     <header class="mb-6">
-        <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
-            {{ $title }}
-        </h2>
+        @if (! $custom)
+            <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+                {{ $title }}
+            </h2>
 
-        <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-            {{ $description }}
-        </p>
+            <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+                {{ $description }}
+            </p>
+        @else
+            <div class="grid grid-cols-2 gap-4">
+                <div>
+                    <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+                        {{ __('Savings Information') }} {{ ($total_saving) == 0 ? '' : '(RM '.$total_saving.')'  }}
+                    </h2>
+                    <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+                        {{ __("Provide your savings information with appropriate amount.") }}
+                    </p>
+                </div>
+                <div class="grid grid-cols-1 place-items-end">
+                    <div>
+                        <x-text-input 
+                            id="balance_after_total_savings" 
+                            class="block mt-1 dark:text-gray-500 text-right" 
+                            type="number" 
+                            name="balance_after_total_savings" 
+                            min="0" 
+                            step="any" 
+                            wire:model="balance" 
+                            :value="old('balance')"  
+                            :disabled="true" 
+                        />
+                    </div>
+                </div>
+            </div>
+        @endif
     </header>
 
     <hr class="h-px my-6 w-80 bg-gray-200 border-0 dark:bg-gray-700">
