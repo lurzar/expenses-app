47de1c03119972df019319d67f91baa7e6d702cb
Lurzar_ <68995865+lurzar@users.noreply.github.com>
2023-03-31 19:09:20 +0800

    Fixed error on header section component

--- resources/views/components/planning-form/section-header.blade.php
+++ resources/views/components/planning-form/section-header.blade.php
@@ -20,10 +20,10 @@
             <div class="grid grid-cols-2 gap-4">
                 <div>
                     <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
-                        {{ __('Savings Information') }} {{ ($total_saving) == 0 ? '' : '(RM '.$total_saving.')'  }}
+                        {{ $title }} {{ ($total_saving) == 0 ? '' : '(RM '.$total_saving.')'  }}
                     </h2>
                     <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-                        {{ __("Provide your savings information with appropriate amount.") }}
+                        {{ $description }}
                     </p>
                 </div>
                 <div class="grid grid-cols-1 place-items-end">
