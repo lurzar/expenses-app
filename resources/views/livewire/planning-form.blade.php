ea2a38fbf39862376d0ace9e728dce2e5c441b3f
Lurzar_ <68995865+lurzar@users.noreply.github.com>
2023-04-02 00:58:23 +0800

    Updated summary section

--- resources/views/livewire/planning-form.blade.php
+++ resources/views/livewire/planning-form.blade.php
@@ -48,29 +48,23 @@
             />
         </div>
 
-        {{-- Planning summary --}}
-        <header class="mb-6">
-            <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
-                {{ __('Planning Summary') }}
-            </h2>
-
-            <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-                {{ __("Summaries of your expenses planning.") }}
-            </p>
-        </header>
-
-        <hr class="h-px my-6 w-80 bg-gray-200 border-0 dark:bg-gray-700">
-
-        <div class="grid grid-cols-2 gap-4 mb-10">
-            <div>
-                <x-input-label for="total-saving" :value="__('Total Saving (RM)')" />
-                <x-text-input id="total-saving" class="block mt-1 w-full" type="text" name="total-saving" :value="old('total-saving')" :disabled="true" />
-                <x-input-error :messages="$errors->get('total-saving')" class="mt-2" />
-            </div>
-            <div>
-                <x-input-label for="total-change" :value="__('Total Change (RM)')" />
-                <x-text-input id="total-change" class="block mt-1 w-full" type="text" name="total-change" :value="old('total-change')" :disabled="true" />
-                <x-input-error :messages="$errors->get('total-change')" class="mt-2" />
+        <div id="summarySection">
+            <x-planning-form.section.header 
+                :title="__('Planning Summary')" 
+                :description="__('Summaries of your expenses planning.')"
+                :custom="false"
+            />
+            <div class="grid grid-cols-2 gap-4 mb-10">
+                <div>
+                    <x-input-label for="total-saving" :value="__('Total Saving (RM)')" />
+                    <x-text-input id="total-saving" class="block mt-1 w-full" type="text" name="total-saving" :value="old('total-saving')" :disabled="true" />
+                    <x-input-error :messages="$errors->get('total-saving')" class="mt-2" />
+                </div>
+                <div>
+                    <x-input-label for="total-change" :value="__('Total Change (RM)')" />
+                    <x-text-input id="total-change" class="block mt-1 w-full" type="text" name="total-change" :value="old('total-change')" :disabled="true" />
+                    <x-input-error :messages="$errors->get('total-change')" class="mt-2" />
+                </div>
             </div>
         </div>
 
