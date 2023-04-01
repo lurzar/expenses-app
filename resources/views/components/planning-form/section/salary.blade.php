b90a9831bde293d190186356e9861a2c72d35353
Lurzar_ <68995865+lurzar@users.noreply.github.com>
2023-04-02 00:35:46 +0800

    Refactor a little bit on salary component

--- resources/views/components/planning-form/section/salary.blade.php
+++ resources/views/components/planning-form/section/salary.blade.php
@@ -1,52 +1,43 @@
-@props(['current_month', 'current_year', 'salary', 'saving_rate'])
-
-<div>
-    <x-planning-form.section-header 
-        :title="__('Salary Information')" 
-        :description="__('Provide your salary information and savings percentages.')"
-    />
-
-    <div class="grid grid-cols-2 gap-4 mb-10">
-        <div>
-            <x-input-label for="current_month" :value="__('Month')" />
-            <x-text-input id="current_month" class="block mt-1 w-full dark:text-gray-500" type="text" name="current_month" :value="$current_month" :disabled="true" required autofocus />
-        </div>
-        <div>
-            <x-input-label for="current_year" :value="__('Year')" />
-            <x-text-input id="current_year" class="block mt-1 w-full dark:text-gray-500" type="text" name="current_year" :value="$current_year" :disabled="true" required autofocus  />
-        </div>
-        <div>
-            <x-input-label for="salary" :value="__('Salary (RM)')" />
-            <x-text-input 
-                id="salary" 
-                class="block mt-1 w-full" 
-                type="number" 
-                name="salary" 
-                min="0.00" 
-                step="any" 
-                wire:model="salary"
-                :value="old('salary')" 
-                placeholder="0.00" 
-                required 
-                autofocus 
-            />
-            <x-input-error :messages="$errors->get('salary')" class="mt-2" />
-        </div>
-        <div>
-            <x-input-label for="saving" :value="__('Saving (%)')" />
-            <x-text-input 
-                id="saving" 
-                class="block mt-1 w-full" 
-                type="number" 
-                name="saving" 
-                min="20" 
-                step="5" 
-                wire:model="saving_rate" 
-                :value="old('saving_rate')"  
-                required 
-                autofocus 
-            />
-            <x-input-error :messages="$errors->get('saving_rate')" class="mt-2" />
-        </div>
+<div class="grid grid-cols-2 gap-4 mb-10">
+    <div>
+        <x-input-label for="current_month" :value="__('Month')" />
+        <x-text-input id="current_month" class="block mt-1 w-full dark:text-gray-500" type="text" name="current_month" wire:model="current_month" :disabled="true" required autofocus />
+    </div>
+    <div>
+        <x-input-label for="current_year" :value="__('Year')" />
+        <x-text-input id="current_year" class="block mt-1 w-full dark:text-gray-500" type="text" name="current_year" wire:model="current_year" :disabled="true" required autofocus  />
+    </div>
+    <div>
+        <x-input-label for="salary" :value="__('Salary (RM)')" />
+        <x-text-input 
+            id="salary" 
+            class="block mt-1 w-full" 
+            type="number" 
+            name="salary" 
+            min="0.00" 
+            step="any" 
+            wire:model="salary"
+            :value="old('salary')" 
+            placeholder="0.00" 
+            required 
+            autofocus 
+        />
+        <x-input-error :messages="$errors->get('salary')" class="mt-2" />
+    </div>
+    <div>
+        <x-input-label for="saving_rate" :value="__('Saving (%)')" />
+        <x-text-input 
+            id="saving_rate" 
+            class="block mt-1 w-full" 
+            type="number" 
+            name="saving_rate" 
+            min="20" 
+            step="5" 
+            wire:model="saving_rate" 
+            :value="old('saving_rate')"  
+            required 
+            autofocus 
+        />
+        <x-input-error :messages="$errors->get('saving_rate')" class="mt-2" />
     </div>
 </div>
