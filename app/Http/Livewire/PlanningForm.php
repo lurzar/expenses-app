c75295be486a05209488169b069458c784482c32
Lurzar_ <68995865+lurzar@users.noreply.github.com>
2023-04-02 15:10:17 +0800

    Added saving fields validation rule

--- app/Http/Livewire/PlanningForm.php
+++ app/Http/Livewire/PlanningForm.php
@@ -30,6 +30,8 @@ class PlanningForm extends Component
     protected $rules = [
         'salary' => 'required|numeric',
         'saving_rate' => 'required|numeric|min:20',
+        'savings_values.*.item' => 'required|string',
+        'savings_values.*.amount' => 'required|numeric',
         'commitments_values.*.item' => 'required|string',
         'commitments_values.*.amount' => 'required|numeric',
         'others_values.*.item' => 'required|string',
