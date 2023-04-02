94b41ed400790b543b218a82f94de2382cea0f83
Lurzar_ <68995865+lurzar@users.noreply.github.com>
2023-04-02 16:04:37 +0800

    Added theme mode stored preferences

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,19 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         @livewireStyles
+
+        <!-- To check stored theme mode preferences -->
+        <script>
+            if (
+              localStorage.getItem('color-theme') === 'dark' ||
+              (!('color-theme' in localStorage) &&
+                window.matchMedia('(prefers-color-scheme: dark)').matches)
+            ) {
+              document.documentElement.classList.add('dark');
+            } else {
+              document.documentElement.classList.remove('dark');
+            }
+        </script>
     </head>
     <body class="font-sans antialiased">
         @livewireScripts
