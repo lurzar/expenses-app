b141568c69ea865b77ef72bdd316c52c55e2fe4b
Lurzar_ <68995865+lurzar@users.noreply.github.com>
2023-04-03 19:14:10 +0800

    Resourced check theme code to app.js

---
+++ resources/js/check-theme.js
@@ -0,0 +1,6 @@
+// To check stored theme mode preferences
+if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    document.documentElement.classList.add('dark');
+} else {
+    document.documentElement.classList.remove('dark');
+}
