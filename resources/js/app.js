46f3aaf6995ff3a7d96142fe3c195567e4449383
Lurzar_ <68995865+lurzar@users.noreply.github.com>
2023-04-03 19:03:07 +0800

    Added fontawesome icon

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,8 @@
 import './bootstrap';
 import './theme';
+import '@fortawesome/fontawesome-free/css/fontawesome.css';
+import '@fortawesome/fontawesome-free/css/brands.css';
+import '@fortawesome/fontawesome-free/css/solid.css';
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
