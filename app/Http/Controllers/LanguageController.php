4a7e1eea12e23d489461fe58928cb14815fed41a
Lurzar_ <68995865+lurzar@users.noreply.github.com>
2023-04-14 19:51:47 +0800

    Added language controller

---
+++ app/Http/Controllers/LanguageController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Session;
+
+class LanguageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+    */
+    public function index(Request $request)
+    {
+        App::setLocale($request->lang);
+        Session::put('locale', $request->lang);
+  
+        return redirect()->back();
+    }
+}
