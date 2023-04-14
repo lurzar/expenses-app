ab87c57243e760bd97804d3129e1c3304441bde4
Lurzar_ <68995865+lurzar@users.noreply.github.com>
2023-04-14 19:51:54 +0800

    Added route to change language

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ExpensesController;
+use App\Http\Controllers\LanguageController;
 use App\Http\Controllers\PlanningController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
@@ -22,21 +23,26 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Dashboard
-Route::get('/dashboard', [DashboardController::class, 'index'])
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+Route::get('/lang/change', [LanguageController::class, 'index'])->name('language');
 
-Route::middleware('auth')->group(function () {
-    // Profile
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    // Planning
-    Route::get('/planning', [PlanningController::class, 'index'])->name('planning.index');
-    Route::post('/planning', [PlanningController::class, 'store'])->name('planning.store');
-    // Expenses
-    Route::get('/expenses', [ExpensesController::class, 'index'])->name('expenses.index');
+Route::group(['middleware'=>'language'],function () {
+    // Dashboard
+    Route::get('/dashboard', [DashboardController::class, 'index'])
+        ->middleware(['auth', 'verified'])
+        ->name('dashboard');
+
+    Route::middleware('auth')->group(function () {
+        // Profile
+        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+        // Planning
+        Route::get('/planning', [PlanningController::class, 'index'])->name('planning.index');
+        Route::post('/planning', [PlanningController::class, 'store'])->name('planning.store');
+        // Expenses
+        Route::get('/expenses', [ExpensesController::class, 'index'])->name('expenses.index');
+    });
+
 });
 
 require __DIR__.'/auth.php';
