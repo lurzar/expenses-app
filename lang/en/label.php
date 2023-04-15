468ee7cb7e434a63403358f84863ca29432ba3ab
Lurzar_ <68995865+lurzar@users.noreply.github.com>
2023-04-15 14:23:27 +0800

    Added label langauge lines

---
+++ lang/en/label.php
@@ -0,0 +1,19 @@
+<?php
+    return [
+        /*
+        |--------------------------------------------------------------------------
+        | English Label Language Lines
+        |--------------------------------------------------------------------------
+        |
+        | The following language lines are used during label form used for various
+        | messages that we need to display to the user. You are free to modify
+        | these language lines according to your application's requirements.
+        |
+        */
+
+        'email' => 'Email',
+        'password' => 'Password',
+        'confirm_password' => 'Confirm Password',
+        'remember_me' => 'Remember me',
+        'name' => 'Name',
+    ];
---
+++ lang/my/label.php
@@ -0,0 +1,19 @@
+<?php
+    return [
+        /*
+        |--------------------------------------------------------------------------
+        | Malay Label Language Lines
+        |--------------------------------------------------------------------------
+        |
+        | The following language lines are used during label form used for various
+        | messages that we need to display to the user. You are free to modify
+        | these language lines according to your application's requirements.
+        |
+        */
+
+        'email' => 'Email',
+        'password' => 'Kata laluan',
+        'confirm_password' => 'Sahkan Kata Laluan',
+        'remember_me' => 'Ingat saya',
+        'name' => 'Nama',
+    ];
