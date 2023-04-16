ec587097dc568813d6b1e49710caa4fa7ae72dc0
Lurzar_ <68995865+lurzar@users.noreply.github.com>
2023-04-16 19:12:22 +0800

    Added icon option props for button component

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,6 +1,25 @@
+@props(['icon' => ''])
+
 <button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150']) }}>
-    <span class="text-sm @if($slot->isNotEmpty()) pr-2 @endif">
-        <i class="fa-solid fa-paper-plane"></i>
-    </span>
-    {{ $slot ?? '' }}
+    @if (! blank($icon))
+        <span class="text-xs @if($slot->isNotEmpty()) pr-2 @endif">
+            @if ($icon == 'save')
+                <i class="fa-solid fa-floppy-disk"></i>
+            @elseif ($icon == 'confirm')
+                <i class="fa-solid fa-circle-check"></i>
+            @elseif ($icon == 'submit')
+                <i class="fa-solid fa-paper-plane"></i>
+            @elseif ($icon == 'login')
+                <i class="fa-solid fa-right-to-bracket"></i>
+            @elseif ($icon == 'update')
+                <i class="fa-solid fa-rotate"></i>
+            @elseif ($icon == 'cancel')
+                <i class="fa-solid fa-ban"></i>
+            @elseif ($icon == 'delete')
+                <i class="fa-solid fa-trash-can"></i>
+            @endif
+        </span>
+    @endif
+
+    {{ $slot }}
 </button>
--- resources/views/components/secondary-button.blade.php
+++ resources/views/components/secondary-button.blade.php
@@ -1,3 +1,25 @@
+@props(['icon' => ''])
+
 <button {{ $attributes->merge(['type' => 'button', 'class' => 'inline-flex items-center px-4 py-2 bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-500 rounded-md font-semibold text-xs text-gray-700 dark:text-gray-300 uppercase tracking-widest shadow-sm hover:bg-gray-50 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 disabled:opacity-25 transition ease-in-out duration-150']) }}>
+    @if (! blank($icon))
+        <span class="text-xs @if($slot->isNotEmpty()) pr-2 @endif">
+            @if ($icon == 'save')
+                <i class="fa-solid fa-floppy-disk"></i>
+            @elseif ($icon == 'confirm')
+                <i class="fa-solid fa-circle-check"></i>
+            @elseif ($icon == 'submit')
+                <i class="fa-solid fa-paper-plane"></i>
+            @elseif ($icon == 'login')
+                <i class="fa-solid fa-right-to-bracket"></i>
+            @elseif ($icon == 'update')
+                <i class="fa-solid fa-rotate"></i>
+            @elseif ($icon == 'cancel')
+                <i class="fa-solid fa-ban"></i>
+            @elseif ($icon == 'delete')
+                <i class="fa-solid fa-trash-can"></i>
+            @endif
+        </span>
+    @endif
+    
     {{ $slot }}
 </button>
