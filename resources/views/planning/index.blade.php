ec05ecd7d4f2c742211e652947a75c2533f2bb00
Lurzar_ <68995865+lurzar@users.noreply.github.com>
2023-04-16 21:00:08 +0800

    Updated index planning page

--- resources/views/planning/index.blade.php
+++ resources/views/planning/index.blade.php
@@ -7,14 +7,18 @@
         </h2>
     </x-slot>
 
-    <div class="my-12">
+    <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     @if ($form_is_unlock)
-                        @livewire('planning', ['close_date' => $form_close_date])
+                        <section id="planningForm">
+                            @livewire('planning', ['close_date' => $form_close_date])
+                        </section>
                     @else
-                        <x-planning-form.lock-banner :open_date="$form_open_date"/>
+                        <section id="planningLock">
+                            <x-planning-form.lock-banner :open_date="$form_open_date"/>
+                        </section>
                     @endif
                 </div>
             </div>
