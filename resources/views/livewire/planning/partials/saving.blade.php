187e95f82da99a1e7791cebb1059abcc0ec1ba1a
Lurzar_ <68995865+lurzar@users.noreply.github.com>
2023-04-21 17:13:06 +0800

    updated saving section

--- resources/views/livewire/planning/partials/saving.blade.php
+++ resources/views/livewire/planning/partials/saving.blade.php
@@ -10,43 +10,45 @@
         &nbsp;
     </x-section-header>
 
+    <x-input-error :messages="$errors->get('savings_amount_limit')" class="mt-2  my-6" />
+
     <div class="grid grid-cols-6 gap-4 mb-6 items-end">
-        @forelse ($savings_items as $savings_item)   
+        @forelse ($savings_fields as $savings_field)   
             <div class="col-span-4">
-                <x-input-label for="savings_values.{{ $savings_item }}.item" :value="__('label.item')" />
+                <x-input-label for="savings_values.{{ $savings_field }}.item" :value="__('label.item')" />
                 <x-text-input 
-                    id="savings_values.{{ $savings_item }}.item" 
+                    id="savings_values.{{ $savings_field }}.item" 
                     class="block mt-1 w-full" 
                     type="text" 
-                    name="savings_values.{{ $savings_item }}.item" 
-                    wire:model="savings_values.{{ $savings_item }}.item" 
-                    :value="old('savings_values.'.$savings_item.'.item')" 
+                    name="savings_values.{{ $savings_field }}.item" 
+                    wire:model="savings_values.{{ $savings_field }}.item" 
+                    :value="old('savings_values.'.$savings_field.'.item')" 
                     placeholder="{{ __('label.saving_placeholder') }}" 
                     required 
                     autofocus 
                 />
-                <x-input-error :messages="$errors->get('savings_values.'.$savings_item.'.item')" class="mt-2" />
+                <x-input-error :messages="$errors->get('savings_values.'.$savings_field.'.item')" class="mt-2" />
             </div>
             <div>
-                <x-input-label for="savings_values.{{ $savings_item }}.amount" :value="__('label.amount')" />
+                <x-input-label for="savings_values.{{ $savings_field }}.amount" :value="__('label.amount')" />
                 <x-text-input 
-                    id="savings_values.{{ $savings_item }}.amount" 
+                    id="savings_values.{{ $savings_field }}.amount" 
                     class="block mt-1 w-full" 
                     type="number" 
-                    name="savings_values.{{ $savings_item }}.amount"
-                    wire:model="savings_values.{{ $savings_item }}.amount"  
+                    name="savings_values.{{ $savings_field }}.amount"
+                    wire:model="savings_values.{{ $savings_field }}.amount"  
                     min="1.00" 
                     step="any" 
-                    :value="old('savings_values.'.$savings_item.'.amount')" 
+                    :value="old('savings_values.'.$savings_field.'.amount')" 
                     placeholder="0.00" 
                     required 
                     autofocus 
                 />
-                <x-input-error :messages="$errors->get('savings_values.'.$savings_item.'.amount')" class="mt-2" />
+                <x-input-error :messages="$errors->get('savings_values.'.$savings_field.'.amount')" class="mt-2" />
             </div>
-            @if ($savings_item != 0)
+            @if ($savings_field != 0)
                 <div>
-                    <x-planning-form.item-remove-button type="button" wire:click="removeSavingItem({{ $savings_item }})" />
+                    <x-planning-form.item-remove-button type="button" wire:click="removeSavingItem({{ $savings_field }})" />
                 </div>
             @endif
         @empty
@@ -56,6 +58,6 @@
         @endforelse
     </div>
     <div class="mb-10">
-        <x-planning-form.item-add-button type="button" wire:click="addSavingItem({{ $savings_items }})" />
+        <x-planning-form.item-add-button type="button" wire:click="addSavingItem({{ $savings_fields }})" />
     </div>
 </div>
