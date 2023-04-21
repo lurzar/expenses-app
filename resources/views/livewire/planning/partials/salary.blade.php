fe34cbbb273774f757bf1a85cce3c9c4c8d126b8
Lurzar_ <68995865+lurzar@users.noreply.github.com>
2023-04-21 20:54:55 +0800

    updated salary section

--- resources/views/livewire/planning/partials/salary.blade.php
+++ resources/views/livewire/planning/partials/salary.blade.php
@@ -12,10 +12,12 @@
         <div>
             <x-input-label for="current_month" :value="__('label.month')" />
             <x-text-input id="current_month" class="block mt-1 w-full dark:text-gray-500" type="text" name="current_month" wire:model="current_month" :disabled="true" required autofocus />
+            <input type="hidden" name="month" id="month" wire:model="current_month">
         </div>
         <div>
             <x-input-label for="current_year" :value="__('label.year')" />
             <x-text-input id="current_year" class="block mt-1 w-full dark:text-gray-500" type="text" name="current_year" wire:model="current_year" :disabled="true" required autofocus  />
+            <input type="hidden" name="month" id="month" wire:model="current_year">
         </div>
         <div>
             <x-input-label for="salary" :value="__('label.salary')" />
@@ -27,7 +29,6 @@
                 min="0.00" 
                 step="any" 
                 wire:model="salary"
-                :value="old('salary')" 
                 placeholder="0.00" 
                 required 
                 autofocus 
@@ -46,7 +47,6 @@
                 max="100" 
                 step="5" 
                 wire:model="saving_rate" 
-                :value="old('saving_rate')"  
                 required 
                 autofocus 
             />
