2b578cf0c70253ff86927a93a19cd6dfdb9ccf93
Lurzar_ <68995865+lurzar@users.noreply.github.com>
2023-04-24 21:08:34 +0800

    Called factory plannning from user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -13,7 +13,12 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->superAdmin()->create();
-        User::factory(10)->unverified()->create();
+        User::factory()->superAdmin()->hasPlannings(10, function (array $attributes, User $user) {
+            return ['user_id' => $user->id];
+        })->create();
+        
+        User::factory(10)->unverified()->hasPlannings(10, function (array $attributes, User $user) {
+            return ['user_id' => $user->id];
+        })->create();
     }
 }
