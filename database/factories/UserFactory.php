58391c1d8542cc79de99d9bafce1a57a5bd23c2c
Lurzar_ <68995865+lurzar@users.noreply.github.com>
2023-04-24 19:09:44 +0800

    Added user factory and seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,7 +22,7 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'remember_token' => null,
         ];
     }
 
@@ -33,6 +33,18 @@ class UserFactory extends Factory
     {
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
+        ]);
+    }
+    /**
+     * Indicate that the model's array should be superadmin.
+     */
+    public function superAdmin(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'name' => 'Super Admin',
+            'email' => 'superadmin@app.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$v93WnEoYslOys3mEosKU/eEBPVGOBmjU.xAaeqUiDT4n.i1Zieh6O' // superadmin@app.com
         ]);
     }
 }
