b5daa6ed054ce9d120de8c971b05bec663a1e7a2
Lurzar_ <68995865+lurzar@users.noreply.github.com>
2023-05-06 00:24:00 +0800

    Fixed

--- resources/views/components/expenses/all.blade.php
+++ resources/views/components/expenses/all.blade.php
@@ -52,57 +52,6 @@
                                         @endforelse
                                     </tbody>
                                 </table>
-                                {{-- <x-modal name="show-expenses" focusable>
-                                    <div class="p-4">
-                                        <div class="mt-6 flex justify-end">
-                                            <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                                                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                                                    <tr>
-                                                        <th scope="col" class="px-6 py-3">
-                                                            Name
-                                                        </th>
-                                                        <th scope="col" class="px-6 py-3">
-                                                            Section
-                                                        </th>
-                                                    </tr>
-                                                </thead>
-                                                <tbody>
-                                                    @forelse ($plannings as $planning)
-                                                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                                                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                                                Planning {{ $planning->month }}, {{ $planning->year }}
-                                                            </th>
-                                                            <td class="px-6 py-4">
-                                                                @forelse ($planning->sections as $key => $section)
-                                                                    {{ $key }}
-                                                                    <br />
-                                                                    @forelse ($section as $key => $item)
-                                                                        {{  $key + 1 }}. Item: {{ $item['item'] }}
-                                                                        <br /> 
-                                                                        &nbsp;&nbsp; Amount: RM{{ $item['amount'] }}
-                                                                    @empty
-                                                                        No section detail found
-                                                                    @endforelse
-                                                                    <br />
-                                                                    <br />
-                                                                @empty
-                                                                    No section data found
-                                                                @endforelse
-                                                            </td>
-                                                        </tr>
-                                                    @empty
-                                                        Data Not Found
-                                                    @endforelse
-                                                </tbody>
-                                            </table>
-                                        </div>
-                                        <div class="mt-6 flex justify-end">
-                                            <x-secondary-button x-on:click="$dispatch('close')" :icon="'cancel'">
-                                                @lang('common.close')
-                                            </x-secondary-button>
-                                        </div>
-                                    </div>
-                                </x-modal> --}}
 
                                 <x-modal name="show-expenses" focusable>
                                     <div class="p-4">
@@ -122,16 +71,16 @@
                                                     @forelse ($planning->sections as $key => $sections)
                                                         <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                                                             <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                                                Planningl {{ $planning->month }}, {{ $planning->year }}
+                                                                Planning {{ $planning->month }}, {{ $planning->year }}
                                                             </th>
                                                             <td class="px-6 py-4">
                                                                 @forelse ($sections as $key => $section)
-                                                                    {{ $key }}
                                                                     <br />
                                                                     @forelse ($section as $key => $item)
                                                                         {{  (int) $key + 1 }}. Item: {{ $item }}
                                                                         <br /> 
                                                                         &nbsp;&nbsp; Amount: RM{{ $item }}
+                                                                        <br /> 
                                                                     @empty
                                                                         No section detail found
                                                                     @endforelse
