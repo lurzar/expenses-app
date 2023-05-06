0a556af14da28a83cb498df9590c6751ea2aa736
Lurzar_ <68995865+lurzar@users.noreply.github.com>
2023-05-06 15:01:45 +0800

    Added show planning page

---
+++ resources/views/planning/show.blade.php
@@ -0,0 +1,19 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            <i class="fa-solid fa-clipboard-list"></i>
+            &nbsp;
+            @lang('common.planning')
+        </h2>
+    </x-slot>
+
+    <div class="pt-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <x-planning.tables.single :plannings="$plannings"/>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
