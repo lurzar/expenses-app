53eb8004c504365ca67fda062e7092337ea82ff7
Lurzar_ <68995865+lurzar@users.noreply.github.com>
2023-05-07 21:49:12 +0800

    Revert

--- resources/views/planning/index.blade.php
+++ resources/views/planning/index.blade.php
@@ -6,7 +6,7 @@
             @lang('common.planning')
         </h2>
     </x-slot>
-{{-- test --}}
+
     <div class="pt-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
