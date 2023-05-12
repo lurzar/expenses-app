cccf35a283f88910446cb3721d816eb4c1bc5ea5
Lurzar_ <68995865+lurzar@users.noreply.github.com>
2023-05-13 02:10:13 +0800

    Modified from single to single-content

--- resources/views/planning/show.blade.php
+++ resources/views/planning/show.blade.php
@@ -11,7 +11,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <x-planning.tables.single :plannings="$plannings"/>
+                    <x-planning.single-content :plannings="$plannings"/>
                 </div>
             </div>
         </div>
