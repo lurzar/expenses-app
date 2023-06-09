00ad63011be5bf3dabb055e3e0bc73912a2878cc
Lurzar_ <68995865+lurzar@users.noreply.github.com>
2023-06-09 23:43:44 +0800

    Update button components

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,7 +1,5 @@
 @props(['icon' => ''])
 
-{{-- @dd($attributes) --}}
-
 <button {{ $attributes->merge(['type' => 'submit', 'class' => 'btn btn-primary']) }}>
     @if (! blank($icon))
         <span class="text-xs @if($slot->isNotEmpty()) pr-2 @endif">
@@ -22,7 +20,9 @@
             @elseif ($icon == 'create')
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-                </svg>              
+                </svg>
+            @else
+                ?      
             @endif
         </span>
     @endif
