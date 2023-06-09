52a6de181986548113d049e921c2c947ad24c3fa
Lurzar_ <68995865+lurzar@users.noreply.github.com>
2023-06-09 23:44:38 +0800

    Updated planning create form

--- resources/views/livewire/planning/index.blade.php
+++ resources/views/livewire/planning/index.blade.php
@@ -1,24 +1,14 @@
 <div>
-    <form action="{{ route('planning.store') }}" method="POST">
-        @csrf
-
-        @include('livewire.planning.partials.salary')
-        @include('livewire.planning.partials.saving')
-        @include('livewire.planning.partials.commitment')
-        @include('livewire.planning.partials.other')
-        @include('livewire.planning.partials.summary')
-        
-        <input type="hidden" name="month" id="month" wire:model="current_month">
-        <input type="hidden" name="year" id="year" wire:model="current_year">
-        <input type="hidden" name="totals[saving]" wire:model="total_savings">
-        <input type="hidden" name="totals[balance]" wire:model="total_balance">
-        <input type="hidden" name="totals[commitment]" wire:model="total_commitment">
-        <input type="hidden" name="totals[other]" wire:model="total_other">
-
-        <div class="mb-6 float-right">
-            <x-primary-button type="submit" :icon="'submit'">
-                @lang('common.submit')
-            </x-primary-button>
-        </div>
-    </form>
+    @include('livewire.planning.partials.salary')
+    @include('livewire.planning.partials.saving')
+    @include('livewire.planning.partials.commitment')
+    @include('livewire.planning.partials.other')
+    @include('livewire.planning.partials.summary')
+    
+    <input type="hidden" name="month" id="month" wire:model="current_month">
+    <input type="hidden" name="year" id="year" wire:model="current_year">
+    <input type="hidden" name="totals[saving]" wire:model="total_savings">
+    <input type="hidden" name="totals[balance]" wire:model="total_balance">
+    <input type="hidden" name="totals[commitment]" wire:model="total_commitment">
+    <input type="hidden" name="totals[other]" wire:model="total_other">
 </div>
--- resources/views/planning/create.blade.php
+++ resources/views/planning/create.blade.php
@@ -9,12 +9,38 @@
 
     <section id="planningCreateSection">
         @if ($form_is_unlock)
-            @livewire('planning', ['close_date' => $form_close_date])
+            <div class="card bg-base-100">
+                <div class="card-body">
+                    <form action="{{ route('planning.store') }}" method="POST">
+                        @csrf
+                        @livewire('planning', ['close_date' => $form_close_date])
+                        <div class="mb-6 float-right">
+                            <x-primary-button type="submit" :icon="'submit'">
+                                @lang('common.submit')
+                            </x-primary-button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         @else
             <div class="hero">
                 <div class="hero-content text-center">
                     <div class="max-w-md">
-                        <h1 class="text-5xl font-bold">
+                        <h1 class="text-5xl font-bold flex">
+                            <svg 
+                                xmlns="http://www.w3.org/2000/svg" 
+                                fill="none" 
+                                viewBox="0 0 24 24" 
+                                stroke-width="1.5" 
+                                stroke="currentColor" 
+                                class="stroke-current h-11 w-11"
+                            >
+                                <path 
+                                    stroke-linecap="round" 
+                                    stroke-linejoin="round" 
+                                    d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" 
+                                />
+                            </svg>
                             Form Locked
                         </h1>
                         <p class="py-6">
