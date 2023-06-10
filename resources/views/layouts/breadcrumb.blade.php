53688f1497b398f6f0491b1a9f907b379e5d2f15
Lurzar_ <68995865+lurzar@users.noreply.github.com>
2023-06-10 16:38:32 +0800

    Added for profile

--- resources/views/layouts/breadcrumb.blade.php
+++ resources/views/layouts/breadcrumb.blade.php
@@ -3,6 +3,7 @@
         'dashboard' => request()->routeIs('dashboard') ?? false,
         'planning' => request()->routeIs('planning.*') ?? false,
         'expenses' => request()->routeIs('expenses.*') ?? false,
+        'profile' => request()->routeIs('profile.*') ?? false,
     ];
     $link = null;
 
@@ -117,6 +118,26 @@
                     </x-nav-link>
                 </li>
             @endif
+        @elseif ($breadcrumbs['profile'])
+            <li>
+                <x-nav-link :href="route('profile.edit')" :active="$breadcrumbs['profile']">
+                    <svg 
+                        xmlns="http://www.w3.org/2000/svg" 
+                        fill="none" 
+                        viewBox="0 0 24 24" 
+                        stroke-width="1.5" 
+                        stroke="currentColor" 
+                        class="w-4 h-4 mr-2 stroke-current"
+                    >
+                        <path 
+                            stroke-linecap="round" 
+                            stroke-linejoin="round" 
+                            d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" 
+                        />
+                    </svg>                      
+                    @lang('common.profile')
+                </x-nav-link>
+            </li>
         @else
             <li>
                 <x-nav-link :href="route('dashboard')" :active="$breadcrumbs['dashboard']">
