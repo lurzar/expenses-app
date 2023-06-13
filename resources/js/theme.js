fae8d4f065339e1a1776da1fe69a9eea46c58b34
Lurzar_ <68995865+lurzar@users.noreply.github.com>
2023-06-13 21:39:42 +0800

    Fixed theme issue

--- resources/js/theme.js
+++ resources/js/theme.js
@@ -1,41 +1,22 @@
 // Handle light mode or dark mode theme
-var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
-var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-btn')
+var themeToggleLightIcon = document.getElementById('theme-toggle-light-btn')
 
 // Change the icons inside the button based on previous settings
-if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    themeToggleLightIcon.classList.remove('hidden');
+if (localStorage.getItem('theme') === 'night' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: night)').matches)) {
+    themeToggleLightIcon.classList.remove('hidden')
 } else {
-    themeToggleDarkIcon.classList.remove('hidden');
+    themeToggleDarkIcon.classList.remove('hidden')
 }
 
-var themeToggleBtn = document.getElementById('theme-toggle');
+themeToggleDarkIcon.addEventListener('click', function() {
+    localStorage.theme = 'night'
+    themeToggleDarkIcon.classList.toggle('hidden')
+    themeToggleLightIcon.classList.remove('hidden')
+});
 
-themeToggleBtn.addEventListener('click', function() {
-
-    // toggle icons inside button
-    themeToggleDarkIcon.classList.toggle('hidden');
-    themeToggleLightIcon.classList.toggle('hidden');
-
-    // if set via local storage previously
-    if (localStorage.getItem('color-theme')) {
-        if (localStorage.getItem('color-theme') === 'light') {
-            document.documentElement.classList.add('dark');
-            localStorage.setItem('color-theme', 'dark');
-        } else {
-            document.documentElement.classList.remove('dark');
-            localStorage.setItem('color-theme', 'light');
-        }
-
-    // if NOT set via local storage previously
-    } else {
-        if (document.documentElement.classList.contains('dark')) {
-            document.documentElement.classList.remove('dark');
-            localStorage.setItem('color-theme', 'light');
-        } else {
-            document.documentElement.classList.add('dark');
-            localStorage.setItem('color-theme', 'dark');
-        }
-    }
-    
+themeToggleLightIcon.addEventListener('click', function() {
+    localStorage.theme = 'cupcake'
+    themeToggleLightIcon.classList.toggle('hidden')
+    themeToggleDarkIcon.classList.remove('hidden')
 });
--- resources/views/components/compiled/theme-option.blade.php
+++ resources/views/components/compiled/theme-option.blade.php
@@ -1,17 +1,16 @@
 <button
-    id="theme-toggle"
-    class="btn btn-ghost btn-circle"
+    id="theme-toggle-dark-btn"
+    class="btn btn-ghost btn-circle hidden"
+    data-key="theme"
+    data-set-theme="night"
 >
     <svg
-        data-key="color-theme"
-        data-set-theme="night"
-        id="theme-toggle-dark-icon"
         xmlns="http://www.w3.org/2000/svg" 
         fill="none" 
         viewBox="0 0 24 24" 
         stroke-width="1.5" 
         stroke="currentColor" 
-        class="w-6 h-6 hidden"
+        class="stroke-current w-6 h-6"
     >
         <path 
             stroke-linecap="round" 
@@ -19,16 +18,20 @@
             d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" 
         />
     </svg>
+</button>
+<button
+    id="theme-toggle-light-btn"
+    class="btn btn-ghost btn-circle hidden"
+    data-key="theme"
+    data-set-theme="cupcake"
+>
     <svg
-        data-key="color-theme"
-        data-set-theme="cupcake"
-        id="theme-toggle-light-icon"
         xmlns="http://www.w3.org/2000/svg" 
         fill="none" 
         viewBox="0 0 24 24" 
         stroke-width="1.5" 
         stroke="currentColor" 
-        class="w-6 h-6 hidden"
+        class="stroke-current w-6 h-6"
     >
         <path 
             stroke-linecap="round" 
